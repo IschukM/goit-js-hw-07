a1117d67922b4166c98c217c7caf296a6d564a5b
IschukM <dzubak@ukr.net>
2022-12-08 07:01:11 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems.reduce(
+  (acc, { preview, original, description }) =>
+    acc +
+    `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`,
+  ""
+);
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+const galleryItem = document.querySelector(".gallery__item");
+console.log(galleryItem);
+// const galleryImage = document.querySelector("gallery__image");
+// galleryItem.addEventListener("click", onClick);
+// function onClick(evt) {
+//   galleryImage.src = "${original}";
+// }
